8dc41bf4b8147d9ef1c146c57c8f87ae2a2a53ec
dbquiroga <db.quiroga93@gmail.com>
2020-01-12 11:51:08 -0300

    added queries in sql

---
+++ queries.sql
@@ -0,0 +1,11 @@
+-- expensive from user 1
+SELECT *
+FROM "expense"
+WHERE user_id = 1
+order by price desc
+
+-- expense by users (suma de gastos por usuario)
+
+-- expense by months (suma de gastos por mes)
+
+-- (suma de gastos por mes por usuario)
